746a46d36df858a48d78ea94445c4c0ffe338741
FawkesOficial <mario.lourenco.morte@gmail.com>
2024-06-13 01:50:21 +0100

    windowrule: made Gimp not floating

--- config.h
+++ config.h
@@ -45,7 +45,7 @@ static const Rule rules[] = {
      *  WM_NAME(STRING) = title
      */
     /* class                instance    title         tags mask    switchtotag    isfloating    monitor */
-    { "Gimp",               NULL,       NULL,         0,           0,             1,            -1 },
+    { "Gimp",               NULL,       NULL,         0,           0,             0,            -1 },
     { "discord",            NULL,       "Discord",    1 << 3,      1,             0,             1 },
     { "obs",                "obs",      "OBS ",       1 << 6,      1,             0,             1 },
     { NULL,                 "cmus",     "cmus",       1 << 7,      1,             0,             1 },
